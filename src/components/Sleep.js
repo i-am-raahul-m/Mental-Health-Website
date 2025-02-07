ac9274d9224811034d20dee55331328adc9086f3
i-am-raahul-m <i.am.raahul.m@gmail.com>
2025-02-07 23:24:50 +0530

    Added to Sleep tab

--- src/components/Sleep.js
+++ src/components/Sleep.js
@@ -3,8 +3,14 @@ import React from 'react';
 function Sleep() {
   const sleepAids = [
     { title: 'White Noise', src: 'White-Noise-60min.mp3' },
-    { title: 'Rain Sounds', src: 'https://example.com/rainsounds.mp3' },
-    { title: 'Sleep Story', src: 'https://example.com/sleepstory.mp3' }
+    { 
+      title: 'Binaural Beats', 
+      src: [
+        'sleep1.mp3',
+        'sleep2.mp3',
+        'sleep3.mp3'
+      ] 
+    }
   ];
 
   return (
@@ -14,18 +20,61 @@ function Sleep() {
         {sleepAids.map((aid, index) => (
           <div key={index} className="sleep-aid">
             <h3>{aid.title}</h3>
-            <audio controls src={aid.src}>
-              Your browser does not support the audio element.
-            </audio>
+            {Array.isArray(aid.src) ? (
+              aid.src.map((song, idx) => (
+                <audio key={idx} controls src={song}>
+                  Your browser does not support the audio element.
+                </audio>
+              ))
+            ) : (
+              <audio controls src={aid.src}>
+                Your browser does not support the audio element.
+              </audio>
+            )}
           </div>
         ))}
+      </div>
+      <div className="youtube-videos">
+        <h3>Relaxing YouTube Videos</h3>
+        <div className="video">
+          <h1>Relaxing Art</h1>
+          <iframe 
+            width="560" 
+            height="315" 
+            src="https://www.youtube.com/embed/SrN4A9rVXj0" 
+            title="YouTube video 1" 
+            frameBorder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowFullScreen
+          ></iframe>
+        </div>
+        <div className="video">
+          <h1>Bed Time Stories</h1>
+          <iframe 
+            width="560" 
+            height="315" 
+            src="https://www.youtube.com/embed/_q5L0JLi0zo" 
+            title="YouTube video 2" 
+            frameBorder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowFullScreen
+          ></iframe>
+        </div>
       </div>
       <div className="sleep-tips">
         <h3>Sleep Tips</h3>
         <ul>
-          <li>Maintain a regular sleep schedule.</li>
-          <li>Create a calming bedtime routine.</li>
-          <li>Avoid screens before bed.</li>
+          <li>Maintain a Consistent Sleep Schedule</li>
+          <li>Create a Restful Environment</li>
+          <li>Limit Exposure to Light Before Bed</li>
+          <li>Engage in Regular Physical Activity</li>
+          <li>Be Mindful of Food and Drink</li>
+          <li>Establish a Relaxing Pre-Sleep Routine</li>
+          <li>Limit Daytime Naps</li>
+          <li>Manage Stress and Anxiety</li>
+          <li>Limit Screen Time Before Bed</li>
+          <li>Optimize Bedroom Temperature</li>
+          <li>Use Comfortable Bedding</li>
         </ul>
       </div>
     </div>
